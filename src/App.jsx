e2a9b52feb61445ef627d701bb5740badc37c6c9
Tuypang Carsco <mailshivanitripathi@gmail.com>
2024-12-29 14:31:34 +0530

    Defining App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
-import { Button } from './components/ui/button';
+import AppLayout from './layouts/aap-layout';
+import LandingPage from './pages/landing';
+import Onboarding from './pages/onboarding';
+import JobListing from './pages/job-listing';
+import JobPage from './pages/job';
+import PostJob from './pages/post-job';
+import SavedJob from './pages/saved-job';
+import MyJobs from './pages/my-jobs';
 
 const router = createBrowserRouter([
   {
@@ -9,13 +16,40 @@ const router = createBrowserRouter([
       {
         path:'/',
         element:<LandingPage />
-      }
+      },
+      
+      {
+        path:'/onboarding',
+        element:<Onboarding />
+      },
+      {
+        path:'/job',
+        element:<JobListing />
+      },
+      {
+        path:'/job/:id',
+        element:<JobPage />
+      },
+      {
+        path:'/post-job',
+        element:<PostJob />
+      },
+      {
+        path:'/saved-job',
+        element:<SavedJob />
+      },
+      {
+        path:'/my-jobs',
+        element:<MyJobs />
+      },
     ]
   }
 ])
 
 function App() {
-  return ();
+  return (
+  <RouterProvider router = {router}/>
+  );
 }
 export default App
 
