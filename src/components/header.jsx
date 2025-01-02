4ce698d6ccd3e5f8534b4d26abd05ef4087c9f21
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-02 16:25:10 +0530

    SupabaseSetup & Header Component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { SignInButton } from '@clerk/clerk-react';
+import { SignedIn } from '@clerk/clerk-react';
+import { SignedOut } from '@clerk/clerk-react';
+import { UserButton } from '@clerk/clerk-react';
 const Header = () => {
   return (
-    <div>Header</div>
+  <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link>
+        <img src='/logo.png' className="h-20"/>
+        </Link>
+
+        <Button variant = "outline">Login</Button>
+        
+        {/*<SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>*/}
+      </nav>
+  </>
   )
 }
 
--- src/layouts/aap-layout.jsx
+++ src/layouts/aap-layout.jsx
@@ -5,10 +5,12 @@ const AppLayout = () => {
   return (
     <div>
     <div className="grid-background"></div>
-    <main className="min-h-screen container px-4 py-6"> 
+    <main className="min-h-screen container px-20 py-4"> 
     <Header/>
     <Outlet/>
     </main>
+    <div className="p-10 text-centre bg-gray-800 mt-10"
+    >Made with 💗 by Tuypang</div>
    </div>
   )
 }
