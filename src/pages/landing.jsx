bc9215791b9ab5471f8553ad495919ed8d511260
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-02 17:43:40 +0530

    Landing Page (44:33)

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
+  return <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+    <section className="text-center">
+      <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
+        Where Potential Meets Purpose {" "}
+        <span className="flex items-center gap-2 sm:gap-6">
+          We {" "}
+          <img 
+            src='/logoW.png'
+            alt='TALENTGRID logo' 
+          className="h-14 sm:h-24 lg:h-32"
+          />
+          </span>
+      </h1>
+      <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+        Explore thousands of job listings or find the perfect candidate.
+      </p>
+    </section>
+    <div className="flex gap-6 justify-center">
+      <Link to="/jobs">
+        <Button variant="blue" size="xl">Find Jobs</Button>
+      </Link>
+      <Link to="/post-job">
+        <Button variant="destructive" size="xl">Post Jobs</Button>
+      </Link>
+   </div>
+    {/*caraousel*/}
+    {/* banner */}
+    
+    <section> 
+      {/* cards */}
+    </section>
+
+    {/* accordion */}
+  </main>
 };
 
 export default LandingPage
