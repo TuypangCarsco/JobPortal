83e50f0beb43555456c56aa0036091a103857c10
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-03 00:41:47 +0530

    Landing Page Completed

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,20 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { Carousel } from "@/components/ui/carousel";
+import { CarouselContent } from "@/components/ui/carousel";
+import { CarouselItem } from "@/components/ui/carousel";
+import companies from '../data/companies.json';
+import faq from '../data/faq.json';
+import Autoplay from "embla-carousel-autoplay";
+import { Card } from "@/components/ui/card";
+import { CardHeader } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
+import { CardTitle } from "@/components/ui/card";
+import { Accordion } from "@/components/ui/accordion";
+import { AccordionContent } from "@/components/ui/accordion";
+import { AccordionItem } from "@/components/ui/accordion";
+import { AccordionTrigger } from "@/components/ui/accordion";
+
 const LandingPage = () => {
   return <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
     <section className="text-center">
@@ -27,11 +42,56 @@ const LandingPage = () => {
       </Link>
    </div>
     {/*caraousel*/}
+    <Carousel plugins={[Autoplay({ delay: 2000})]}       
+     className="w-full py-10">
+      <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+        {companies.map(({name, id, path})=>{
+        return (
+        <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+          <img src={path}  
+          alt={name}
+          className="h-9 sm:h-14 object-contain"/>
+        </CarouselItem>
+      );
+    })}
+      </CarouselContent>
+    </Carousel>
+
     {/* banner */}
+    <img src="/banner.jpg" alt="Banner" style={{ width: '100%', height: 'auto' }} className="w-full"/>
     
-    <section> 
-      {/* cards */}
-    </section>
+  <section className="grid grid-cols-1 md:grid-cols-2 gap-4"> 
+  <Card>
+  <CardHeader>
+    <CardTitle>For Job Seekers</CardTitle>
+    </CardHeader>
+  <CardContent>
+    <p>
+      Search and apply for jobs, track applications, and more.
+    </p>
+  </CardContent>
+ </Card>
+ <Card>
+  <CardHeader>
+    <CardTitle>For Employers</CardTitle>
+    </CardHeader>
+  <CardContent>
+    <p>Post jobs, manage applications, and find best candidates.</p>
+  </CardContent>
+ </Card>
+
+  {/* cards */}
+  </section>
+  <Accordion type="single" collapsible>
+    {faq.map((faq,index)=>{
+ return(
+  <AccordionItem key={index} value={`item-${index+1}`}>
+   <AccordionTrigger>{faq.question}</AccordionTrigger>
+  <AccordionContent>{faq.answer}</AccordionContent>
+ </AccordionItem>
+ );
+    })}
+ </Accordion>
 
     {/* accordion */}
   </main>
