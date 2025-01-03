d64a87083981aaa67e8348c11405ca8d93e9cf73
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-03 21:57:09 +0530

    importing User Authentication

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
 import { SignInButton } from '@clerk/clerk-react';
 import { SignedIn } from '@clerk/clerk-react';
 import { SignedOut } from '@clerk/clerk-react';
 import { UserButton } from '@clerk/clerk-react';
+import { PenBox } from 'lucide-react';
+import { SignIn } from '@clerk/clerk-react';
+
 const Header = () => {
+const[showSignIn, setShowSignIn]= useState(false);
+
   return (
   <>
       <nav className="py-4 flex justify-between items-center">
@@ -13,15 +18,30 @@ const Header = () => {
         <img src='/logo.png' className="h-20"/>
         </Link>
 
-        <Button variant = "outline">Login</Button>
-        
-        {/*<SignedOut>
-        <SignInButton />
+       
+      <div className="flex gap-8">
+      <SignedOut>
+          <Button variant = "outline">Login</Button>
       </SignedOut>
+      
       <SignedIn>
+        {/*add a condn */}
+      <Button variant='destructive' className='rounded-full'>
+        <PenBox size={20} className="mr-2"/>
+        Post a Job
+      </Button>
+        <Link to='/post-job'></Link>
         <UserButton />
-      </SignedIn>*/}
+      </SignedIn>
+      </div>
       </nav>
+
+      {showSignIn && (<div>
+        <SignIn
+        signUpForceRedirectUrl='/onboarding'
+        fallbackRedirectUrl='/onboarding'
+        />
+      </div> )}
   </>
   )
 }
