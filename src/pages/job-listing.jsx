fc8a93fa761244e510ce9654f30027bc16ce159e
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-15 16:40:49 +0530

    JobListing

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,7 +1,59 @@
-const JobListing = () => {
-  return (
-    <div>JobListing</div>
-  )
-};
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { State } from "country-state-city";
+import { BarLoader } from "react-spinners";
+import useFetch from "@/hooks/use-fetch";
 
-export default JobListing
+import JobCard from "@/components/job-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { getCompanies } from "@/api/apiCompanies";
+import { getJobs } from "@/api/apijobs";
+
+const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+
+  const { isLoaded } = useUser();
+
+  const {
+    // loading: loadingCompanies,
+    data: companies,
+    fn: fnCompanies,
+  } = useFetch(getCompanies);
+
+  const {
+    loading: loadingJobs,
+    data: jobs,
+    fn: fnJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnCompanies();
+    }
+   
+  }, [isLoaded]);
+
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  return <div></div>;
+};
+export default JobListing;
