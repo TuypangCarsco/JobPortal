7d4d7105b08d126bb47003fa61b5a23bf9ee210b
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-17 17:20:44 +0530

    JobListing_new

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,59 +1,54 @@
-import { useEffect, useState } from "react";
-import { useUser } from "@clerk/clerk-react";
-import { State } from "country-state-city";
-import { BarLoader } from "react-spinners";
+import { getJobs } from "@/api/apijobs"
 import useFetch from "@/hooks/use-fetch";
-
-import JobCard from "@/components/job-card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import { getCompanies } from "@/api/apiCompanies";
-import { getJobs } from "@/api/apijobs";
+import { useSession, useUser } from "@clerk/clerk-react"
+import { useEffect, useState } from "react"
+import { BarLoader } from "react-spinners";
 
 const JobListing = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [location, setLocation] = useState("");
-  const [company_id, setCompany_id] = useState("");
-
-  const { isLoaded } = useUser();
-
-  const {
-    // loading: loadingCompanies,
-    data: companies,
-    fn: fnCompanies,
-  } = useFetch(getCompanies);
-
-  const {
-    loading: loadingJobs,
-    data: jobs,
-    fn: fnJobs,
-  } = useFetch(getJobs, {
+ const [searchQuery, setSearchQuery] = useState("");
+ const [location, setLocation] = useState("");
+ const [company_id, setCompany_id] = useState("");
+  const {isLoaded} = useUser()
+  
+  const{
+    data:jobs, 
+    loading:loadingJobs, 
+    fn: fnJobs
+  } = useFetch(getJobs,{
     location,
     company_id,
     searchQuery,
   });
 
-  useEffect(() => {
-    if (isLoaded) {
-      fnCompanies();
-    }
-   
-  }, [isLoaded]);
+  console.log(jobs);
 
-  useEffect(() => {
-    if (isLoaded) fnJobs();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoaded, location, company_id, searchQuery]);
+  useEffect(()=>{
+    if(isLoaded) fnJobs();
+  },[isLoaded, location, company_id, searchQuery]);
 
-  return <div></div>;
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  
+  return <div>
+    <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">Latest Jobs</h1>
+    {/*add filters */}
+    
+    {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+    )}
+     {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return <span>{job.title}</span>
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+  </div>
 };
-export default JobListing;
+
+export default JobListing
